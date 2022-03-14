9bf3613737c3136e9608f0bf16e3ce3bc5bc3726
joko28 <jozey.nguyen@sjsu.edu>
2022-03-14 11:11:50 -0700

    works on p5.js but not on live server

--- sketch.js
+++ sketch.js
@@ -6,65 +6,66 @@
 //Preload runs before setup
 
 let wolves = [{
-  name: "wolf",
+  name: "WOLF",
   color: "pink"
 }, {
-  name: "zoe",
+  name: "ZOE",
   color: "purple"
 }, {
-  name: "dakota",
+  name: "DAKOTA",
   color: "brown"
 }, {
-  name: "star",
+  name: "STAR",
   color: "yellow"
 }, {
-  name: "crystal",
+  name: "CRYSTAL",
   color: "gray"
 }, {
-  name: "luna",
+  name: "LUNA",
   color: "gray"
 }];
 
 let randomIndex;
 
 //function preload() {
-  //for (i = 0; i <= 20; i++) {
-  //  wolvesImg[i] = loadImage('wolves/'+[i]+'.jpg');
-  //}
+//for (i = 0; i <= 20; i++) {
+//  wolvesImg[i] = loadImage('wolves/'+[i]+'.jpg');
+//}
 //}
 function setup() {
   creatCanvas(1000, 1000);
+  background('pink');
+  textSize(32);
+  text("click to randomize", 500, 500);
   //let canvas = createCanvas(1000, 1000);
   //canvas.parent('myCanvas');
   //frameRate(30);
   //imageMode(CENTER);
 
-  textSize(32);
   //console.log(wolves[2]);
 }
 
 function draw() {
-  background('pink');
   //if (frameCount % 150 == 0) {
   //  img = int(random(wolvesImg.length));
   //}
   //image(wolvesImg[img], width/2, height/2.5, 400, 711);
-  //drawCloud();
+  drawCloud();
 }
 
 function drawCloud() {
-  push();
-  fill(255);
-  noStroke();
-  ellipse(250, 250, 350, 150);
-  ellipse(180, 200, 130, 100);
-  ellipse(320, 220, 140, 130);
-  ellipse(250, 190, 120, 100);
-  pop();
+push();
+fill(255);
+noStroke();
+ellipse(250, 250, 350, 150);
+ellipse(180, 200, 130, 100);
+ellipse(320, 220, 140, 130);
+ellipse(250, 190, 120, 100);
+pop();
 }
 
-function mousePressed(){
-  if (wolves[0]){
+function mousePressed() {
+  if (wolves[0]) {
     //this displays random name & splices it out of array
     background(random(200, 255));
     randomIndex = int(random(wolves.length));
